f39c7fe3dd4089cf9e8a4a1f2e003705435d89bc
William WATKINS <wwatkins@e2r10p19.42.fr>
2018-06-15 12:44:16 +0200

    wip

--- shader/fragment/raymarch_frag.glsl
+++ shader/fragment/raymarch_frag.glsl
@@ -56,6 +56,7 @@ const float minDistShadow = 0.005;  // the distance from object threshold at whi
 
 /* prototypes */
 vec4    raymarch( in vec3 ro, in vec3 rd, float s );
+// float   raymarchVolume( in vec3 ro, in vec3 rd, float ct, float s );
 vec3    getNormal( in vec3 p );
 vec3    computeDirectionalLight( int objId, in vec3 hit, in vec3 normal, in vec3 viewDir, in vec3 m_diffuse, bool use_shadows, bool use_occlusion );
 float   softShadow( in vec3 ro, in vec3 rd, float mint, float k );
@@ -64,6 +65,7 @@ vec3    map( in vec3 p );
 float   fbm2d(in vec2 st, in float amplitude, in float frequency, in int octaves, in float lacunarity, in float gain);
 float   fbm3d(in vec3 st, in float amplitude, in float frequency, in int octaves, in float lacunarity, in float gain);
 
+vec2    raySphere( in vec3 ro, in vec3 rd, in vec4 sph );
 float   cloud( vec3 p );
 float   sphere( vec3 p, float s );
 float   cube( vec3 p );
@@ -82,11 +84,7 @@ float random(vec2 p) {
     return fract(sin(dot(p.xy, vec2(12.9898,78.233)))*43758.5453123);
 }
 
-float random(vec3 p) {
-	// p = vec3( dot(p,vec3(127.1,311.7, 74.7)),
-	// 		  dot(p,vec3(269.5,183.3,246.1)),
-	// 		  dot(p,vec3(113.5,271.9,124.6)));
-	// return -1.0 + 2.0*fract(sin(p)*43758.5453123);
+float random(vec3 p) { // find better random ?
 	return fract(sin(dot(p, vec3(113.5,271.9,124.6)))*43758.5453123);
 }
 
@@ -140,6 +138,23 @@ float pattern(vec2 st, vec2 v, float t) {
     return step(t, random(100.+p*.000001)+random(p.x)*0.5 );
 }
 
+// NEW
+float    raymarchVolume( in vec3 ro, in vec3 rd, in vec2 bounds, float s ) { // same as raymarch, but we accumulate value when inside and sample the occlusion
+    const int maxVolumeSamples = 32; // max steps in sphere
+    float stepSize = 1.0 / float(maxVolumeSamples); // granularity
+	float t = bounds.x + stepSize;
+    float acc = 0.0;
+	for (int i = 0; i < maxVolumeSamples; i++) {
+        vec3 pos = ro + rd * t;
+        float se = fbm3d(pos, 1.5, 2.0, 16, 2.5, 0.5);
+        acc += se * stepSize;
+        if (t > bounds.y) break; // if we go out of sphere
+        if (t > maxDist || t > s) break; // optimization and geometry occlusion
+		t += stepSize;
+	}
+	return acc;
+}
+
 void    main() {
     vec2 uv = vec2(TexCoords.x, 1.0 - TexCoords.y);
     vec3 ndc = vec3(uv * 2.0 - 1.0, -1.0);
@@ -153,82 +168,107 @@ void    main() {
     depth = 2.0 * Near * Far / (Far + Near - depth * (Far - Near));
 
     // raymarch
-    vec4 res = raymarch(cameraPos, dir, depth);
-    if (res.x == 0.0) { FragColor = vec4(0.0); return ; }
+    // vec4 res = raymarch(cameraPos, dir, depth);
+    // if (res.x == 0.0) { FragColor = vec4(0.0); return ; }
 
-    int id = int(res.w);
+    // int id = int(res.w);
 
     // compute useful variables for light
-    vec3 hit = cameraPos + dir * res.x;
-    vec3 normal = getNormal(hit);
-    vec3 viewDir = normalize(cameraPos - hit);
-
-    // FragColor = vec4(vec3(1.0) * fbm2d(hit.xy+uTime*0.05, 0.5, 10.0*(sin(uTime)*0.5+0.5), 16, 4.0, 0.5), 1.0);
-    // FragColor = vec4(vec3(1.0) * fbm2d(TexCoords.xy, 0.5, 10.0, 16, 4.0, 0.5), 1.0);
-
-    // float r = fbm2d(TexCoords.xy + fbm2d(TexCoords.xy + uTime*0.1, 0.85, 4.0, 16, 4.0, 0.5), 0.5, 1.0, 16, 8.0, 0.65);
-    // FragColor = vec4(vec3(1.0) * pow(r, 10.0), 1.0);
+    // vec3 hit = cameraPos + dir * res.x;
+    // vec3 normal = getNormal(hit);
+    // vec3 viewDir = normalize(cameraPos - hit);
 
     // FragColor = vec4(vec3(1.0) * fbm3d(hit+uTime*0.015, 0.5, 10.0, 16, 4.0, 0.5), 1.0-fbm3d(hit+uTime*0.05, 0.5, 10.0, 16, 4.0, 0.5));
     // FragColor = vec4(vec3(1.0) * fbm3d(vec3(TexCoords.xy, uTime*0.05), 0.5, 10.0, 10, 2.5, 0.5), 1.0);
-    FragColor = vec4(vec3(1.0) * fbm3d(hit+uTime*0.01, 0.5, 2.0, 16, 2.5, 0.5), 1.0);
+    // FragColor = vec4(vec3(1.0) * fbm3d(hit+uTime*0.01, 0.5, 2.0, 16, 2.5, 0.5), 1.0);
+    
+    // NEW
+    vec2 bounds = raySphere(cameraPos, dir, vec4(0.0, 0.0, 0.0, 1.0));
+    float res = raymarchVolume(cameraPos, dir, bounds, depth);
+    res = 1.0 / exp(res * 0.5);
+    vec3 color = vec3(1.0) * (1.0 - res) * 1.0 / float(32);
+    FragColor = vec4(color, res);
+	// float t = 0.0;
+    // float acc = 0.0;
+	// for (int i = 0; i < maxRaySteps; i++) {
+    //     float res = map(cameraPos + dir * t).x;
+	// 	t += res;
+    //     if (t > maxDist || t > depth) {
+    //         FragColor = vec4(0.0);
+    //         return;
+    //     }
+	// 	if (res < minDist) {
+    //         // acc = raymarchVolume(cameraPos, dir, t, depth);
+    //         acc = raymarchVolume(cameraPos+dir*t, dir, t, depth);
+    //         acc = 1.0 / exp(acc * 0.95);
+    //         vec3 color = vec3(1.0) * (1.0 - acc) + vec3(1.0) * acc;
+    //         FragColor = vec4(color, acc * 0.5);
+    //         return;
+    //     }
+	// }
+    // // acc = raymarchVolume(cameraPos, dir, t, depth);
+    // acc = raymarchVolume(cameraPos+dir*t, dir, t, depth);
+    // acc = 1.0 / exp(acc * 0.5);
+    // vec3 color = vec3(1.0) * (1.0 - acc) + vec3(1.0) * acc;
+    // FragColor = vec4(color, acc * 0.5);
+    // END
     return;
 
     // compute colors
-    if (object[id].id == 0) { // mandelbox
-        float it = res.z / float(maxRaySteps);
-        vec3 color = (vec3(0.231, 0.592, 0.776) + res.y * res.y * vec3(0.486, 0.125, 0.125)) * 0.3;
-        vec3 light = computeDirectionalLight(id, hit, normal, viewDir, vec3(0.898, 0.325, 0.7231), false, false);
-        // NEW
-        vec2 st = hit.xy * 10;
-        vec2 grid = vec2(100.0, 50.0);
-        st *= grid;
-        vec2 ipos = floor(st);
-        vec2 fpos = fract(st);
-        vec2 vel = vec2(uTime * 0.3 * max(grid.x, grid.y));
-        vel *= vec2(-1.0, 0.0) * random(1.0 + ipos.y);
-        vec2 offset = vec2(0.1, 0.);
-        vec3 patt = vec3(0.);
-        patt.r = pattern(st + offset, vel, 0.5 + 0.1);
-        patt.g = pattern(st, vel, 0.5 + 0.1);
-        patt.b = pattern(st - offset, vel, 0.5 + 0.1);
-        patt *= step(0.2, fpos.y);
-        patt = (1.0-patt * 0.7) * vec3(1.0, 0.73, 0.3) * 1.5;
+    // if (object[id].id == 0) { // mandelbox
+    //     float it = res.z / float(maxRaySteps);
+    //     vec3 color = (vec3(0.231, 0.592, 0.776) + res.y * res.y * vec3(0.486, 0.125, 0.125)) * 0.3;
+    //     vec3 light = computeDirectionalLight(id, hit, normal, viewDir, vec3(0.898, 0.325, 0.7231), false, false);
+    //     // NEW
+    //     vec2 st = hit.xy * 10;
+    //     vec2 grid = vec2(100.0, 50.0);
+    //     st *= grid;
+    //     vec2 ipos = floor(st);
+    //     vec2 fpos = fract(st);
+    //     vec2 vel = vec2(uTime * 0.3 * max(grid.x, grid.y));
+    //     vel *= vec2(-1.0, 0.0) * random(1.0 + ipos.y);
+    //     vec2 offset = vec2(0.1, 0.);
+    //     vec3 patt = vec3(0.);
+    //     patt.r = pattern(st + offset, vel, 0.5 + 0.1);
+    //     patt.g = pattern(st, vel, 0.5 + 0.1);
+    //     patt.b = pattern(st - offset, vel, 0.5 + 0.1);
+    //     patt *= step(0.2, fpos.y);
+    //     patt = (1.0-patt * 0.7) * vec3(1.0, 0.73, 0.3) * 1.5;
 
-        // float r = 2.0*fbm2d(hit.xy + fbm2d(hit.xy + uTime*0.01, 0.85, 3.0, 16, 3.0, 0.5), 0.85, 3.0, 16, 6.0, 0.5);
+    //     // float r = 2.0*fbm2d(hit.xy + fbm2d(hit.xy + uTime*0.01, 0.85, 3.0, 16, 3.0, 0.5), 0.85, 3.0, 16, 6.0, 0.5);
 
-        FragColor = vec4(patt * light * color * vec3(0.9, 0.517, 0.345) * 1.2, -log(it)*2.0);
-        /* add fog if we're in cube */
-        float fog = res.x * 0.5 / object[id].scale * 12.0;
-        vec3 colorFog = fog * fog * vec3(0.9, 0.517, 0.345) * 0.5;
-        float t = 0.0;
-        for (int i = 0; i < 10; i++) {
-            float scale = object[id].scale;
-            vec3 p = (object[id].invMat * vec4(cameraPos + dir * t, 1.0)).xyz / scale;
-            float res = cube(p) * scale;
-            t += res;
-            if (t > maxDist || t > depth) break;
-            if (res < 0.1*object[id].scale) { FragColor.xyz += clamp(colorFog * (0.1-t), 0.0, 10.0); break; }
-        }
-    }
-    else if (object[id].id == 1) { // mandelbulb
-        res.y = pow(clamp(res.y, 0.0, 1.0), 0.55);
-        vec3 tc0 = 0.5 + 0.5 * sin(3.0 + 4.2 * res.y + vec3(0.0, 0.5, 1.0));
-        vec3 diffuse = vec3(0.9, 0.8, 0.6) * 0.2 * tc0 * 8.0;
-        vec3 color = computeDirectionalLight(id, hit, normal, viewDir, diffuse, true, true);
-        FragColor = vec4(color, object[id].material.opacity);
-    }
-    else if (object[id].id == 2) { // ifs
-        float g = pow(2.0 + res.z / float(maxRaySteps), 4.0) * 0.05;
-        vec3 glow = vec3(1.0 * g, 0.819 * g * 0.9, 0.486) * g * 2.0;
-        vec3 diffuse = vec3(1.0, 0.694, 0.251);
-        vec3 light = computeDirectionalLight(id, hit, normal, viewDir, diffuse, true, false);
-        FragColor = vec4(light + log(glow * 0.95) * 0.75, 1.0);
-    }
-    else {
-        vec3 color = computeDirectionalLight(id, hit, normal, viewDir, object[id].material.diffuse, true, true);
-        FragColor = vec4(color, object[id].material.opacity);
-    }
+    //     FragColor = vec4(patt * light * color * vec3(0.9, 0.517, 0.345) * 1.2, -log(it)*2.0);
+    //     /* add fog if we're in cube */
+    //     float fog = res.x * 0.5 / object[id].scale * 12.0;
+    //     vec3 colorFog = fog * fog * vec3(0.9, 0.517, 0.345) * 0.5;
+    //     float t = 0.0;
+    //     for (int i = 0; i < 10; i++) {
+    //         float scale = object[id].scale;
+    //         vec3 p = (object[id].invMat * vec4(cameraPos + dir * t, 1.0)).xyz / scale;
+    //         float res = cube(p) * scale;
+    //         t += res;
+    //         if (t > maxDist || t > depth) break;
+    //         if (res < 0.1*object[id].scale) { FragColor.xyz += clamp(colorFog * (0.1-t), 0.0, 10.0); break; }
+    //     }
+    // }
+    // else if (object[id].id == 1) { // mandelbulb
+    //     res.y = pow(clamp(res.y, 0.0, 1.0), 0.55);
+    //     vec3 tc0 = 0.5 + 0.5 * sin(3.0 + 4.2 * res.y + vec3(0.0, 0.5, 1.0));
+    //     vec3 diffuse = vec3(0.9, 0.8, 0.6) * 0.2 * tc0 * 8.0;
+    //     vec3 color = computeDirectionalLight(id, hit, normal, viewDir, diffuse, true, true);
+    //     FragColor = vec4(color, object[id].material.opacity);
+    // }
+    // else if (object[id].id == 2) { // ifs
+    //     float g = pow(2.0 + res.z / float(maxRaySteps), 4.0) * 0.05;
+    //     vec3 glow = vec3(1.0 * g, 0.819 * g * 0.9, 0.486) * g * 2.0;
+    //     vec3 diffuse = vec3(1.0, 0.694, 0.251);
+    //     vec3 light = computeDirectionalLight(id, hit, normal, viewDir, diffuse, true, false);
+    //     FragColor = vec4(light + log(glow * 0.95) * 0.75, 1.0);
+    // }
+    // else {
+    //     vec3 color = computeDirectionalLight(id, hit, normal, viewDir, object[id].material.diffuse, true, true);
+    //     FragColor = vec4(color, object[id].material.opacity);
+    // }
 
     // iterations color
     // {
@@ -273,19 +313,6 @@ vec3    map( in vec3 p ) {
     }
     return res;
 }
-
-// vec4    raymarchVolume( in vec3 ro, in vec3 rd, float s ) { // same as raymarch, but we accumulate value when inside and sample the occlusion
-//     const int volumeSamples = 256;
-//     float stepSize = 0.01; // granularity
-// 	float t = 0.0;
-// 	for (int i = 0; i < volumeSamples; i++) {
-//         float sample = fbm3d(ro + rd * t, 0.5, 2.0, 16, 2.5, 0.5);
-// 		t += stepSize;
-//         if (t > maxDist || t > s) return vec4(0.0, 0.0, i, 0.0); // optimization and geometry occlusion
-// 		if (res.x < minDist) return vec4(t, res.y, i, res.z);
-// 	}
-// 	return vec4(0.0, 0.0, maxRaySteps, 0.0);
-// }
 
 /* return: distance, trap, ray_iterations, object_id */
 vec4    raymarch( in vec3 ro, in vec3 rd, float s ) {
@@ -374,11 +401,23 @@ float fbm3d(in vec3 st, in float amplitude, in float frequency, in int octaves, 
 
 // fbm(pos, 0.5, 1.0, 8, 3.0, 0.5);
 
+
+// returns the min/max dist if intersecting with the sphere (sph is a vec4 with xyz being pos and w the size)
+vec2 raySphere( in vec3 ro, in vec3 rd, in vec4 sph ) {
+	vec3 oc = ro - sph.xyz;
+	float b = dot(oc, rd);
+	float c = dot(oc, oc) - sph.w*sph.w;
+	float h = b*b - c;
+	if (h < 0.0) return vec2(-1.0);
+	h = sqrt(h);
+	return vec2(-b-h, -b+h);
+}
+
 /*  Distance Estimators
 */
 
 float   cloud( vec3 p ) {
-    return sphere(p, 10.0);
+    return sphere(p, 20.0);
 }
 
 float   sphere( vec3 p, float s ) {
